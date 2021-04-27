9be02e83878c39151b525354d762ac3dd225e6b3
VulpineGorgon <kgaerdes@gmail.com>
2021-04-27 12:34:25 -0400

    updated config for firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,13 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+    apiKey: "AIzaSyDlYsrUbVSoSHgUUGONc57V3GvfibU4CBY",
+    authDomain: "prog8110winter2021-c4be4.firebaseapp.com",
+    projectId: "prog8110winter2021-c4be4",
+    storageBucket: "prog8110winter2021-c4be4.appspot.com",
+    messagingSenderId: "72593607138",
+    appId: "1:72593607138:web:35c357a486a18cd60a51bb",
+    measurementId: "G-NCJVYW9KMQ"
 };
 
 if (!firebase.apps.length) {
